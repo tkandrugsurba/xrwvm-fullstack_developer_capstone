252774bff406f8c595bfa244075e2c371fe5d0b6
tkandrugsurba <tkandrugsurba@gmail.com>
2024-12-23 02:00:03 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import LoginPanel from "./components/Login/Login";
 
 function App() {
   return (
     <Routes>
+      {/* Configures the /login route to render the LoginPanel component */}
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
+
 export default App;
