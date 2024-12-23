02783eeacc616588f5f74cee24bd4b92aee669a9
tkandrugsurba <tkandrugsurba@gmail.com>
2024-12-23 18:30:31 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,18 +14,22 @@ urlpatterns = [
     # Path for login - updated to serve the React page
     path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
 
-    # Path for dealer reviews view
-
-    # Path for add a review view
-
+    # Path for logout
     path('logout/', view=views.logout_request, name='logout'),
-    path('get_cars/', view=views.get_cars, name='getcars'),  # Added the get_cars path
+
+    # Path for getting all cars
+    path('get_cars/', view=views.get_cars, name='getcars'),
+
+    # Paths for getting dealers
     path('get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path('get_dealers/<str:state>/',
-         view=views.get_dealerships, name='get_dealers_by_state'),
-    path('dealer/<int:dealer_id>/',
-         view=views.get_dealer_details, name='dealer_details'),
-    path('reviews/dealer/<int:dealer_id>/',
-         view=views.get_dealer_reviews, name='dealer_details'),
+    path('get_dealers/<str:state>/', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # Path for getting dealer details
+    path('dealer/<int:dealer_id>/', view=views.get_dealer_details, name='dealer_details'),
+
+    # Path for getting dealer reviews
+    path('reviews/dealer/<int:dealer_id>/', view=views.get_dealer_reviews, name='dealer_reviews'),
+
+    # Path for adding a review
     path('add_review/', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
