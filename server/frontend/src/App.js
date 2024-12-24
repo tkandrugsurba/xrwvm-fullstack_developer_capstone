b4362d41e36ebc8afad79096e508c6ef592dc5d3
tkandrugsurba <tkandrugsurba@gmail.com>
2024-12-24 01:34:27 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
-import Register from "./components/Register/Register"; // Import the Register component
+import Register from "./components/Register/Register";
+import Dealers from "./components/Dealers/Dealers";
+import Dealer from "./components/Dealers/Dealer"; // Import the Dealer component
 
 function App() {
   return (
     <Routes>
       {/* Configures the /login route to render the LoginPanel component */}
       <Route path="/login" element={<LoginPanel />} />
-      
+
       {/* Configures the /register route to render the Register component */}
       <Route path="/register" element={<Register />} />
+
+      {/* Configures the /dealers route to render the Dealers component */}
+      <Route path="/dealers" element={<Dealers />} />
+
+      {/* Configures the /dealer/:id route to render the Dealer component */}
+      <Route path="/dealer/:id" element={<Dealer />} />
     </Routes>
   );
 }
